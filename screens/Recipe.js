a163554282ddb2e396062d82c16069c075150194
Siya Choudhary <siyacanvas@gmail.com>
2025-05-04 20:47:26 -0500

    UI changes

---
+++ screens/Recipe.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet, Alert, Platform, TouchableOpacity } from "react-native";
+import firestore from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Recipe() {
+
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
